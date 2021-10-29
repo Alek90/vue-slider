0a01b335153ef34a0b14e1d394d082b1e2be8677
Alek90 <4lek.90@gmail.com>
2021-10-29 12:45:14 +0200

    Impostata struttura Vue.js dell'array di oggetti. Da compilare.

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,32 @@
 const root = new Vue({
     el: "#root",
     data: {
-        
+        countries: [
+            {
+                image:"",
+                name:"",
+                caption:""
+            },
+            {
+                image:"",
+                name:"",
+                caption:""
+            },
+            {
+                image:"",
+                name:"",
+                caption:""
+            },
+            {
+                image:"",
+                name:"",
+                caption:""
+            },
+            {
+                image:"",
+                name:"",
+                caption:""
+            }
+        ]
     }
 })
